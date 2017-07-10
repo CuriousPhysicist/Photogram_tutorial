2448abfdc1bb5b4e06ddccfe5179ba7e8f65e9ca
CuriousPhysicist <andrewhampson6@gmail.com>
2017-07-10 13:43:54 +0100

    adds flash messages to application view.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,10 @@ class PostsController < ApplicationController
 
 	def create
 		if @post = Post.create(post_params)
+			flash[:success] = "Your post has been created!"
 			redirect_to posts_path
 		else
+			flash.now[:alert] = "Your post couldn't be created! Please check the form"
 			render :new
 		end
 	end
@@ -26,15 +28,22 @@ class PostsController < ApplicationController
 
 	def update
 		if @post.update(post_params)
+			flash[:success] = "Post updated."
 			redirect_to posts_path
 		else
+			flash.now[:alert] = "Update failed. Please check the form"
 			render :edit
 		end
 	end
 
 	def destroy
-		@post.destroy
-		redirect_to posts_path
+		if @post.destroy
+			flash[:notice] = "Your post was deleted."
+			redirect_to posts_path
+		else
+			flash.now[:error] = "Post failed to be removed. Please try again."
+			render :edit
+		end
 
 	end
 
