c8cfd7f7461adf9849ff2f408854b5239bd28701
CuriousPhysicist <andrewhampson6@gmail.com>
2017-07-17 22:15:39 +0100

    Adds 2 users to the db seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+		user_name: "CuriousPhysicist",
+		email: "user@example.com",
+		password: "Curiosity"
+	)
+
+User.create!(
+		user_name: "Guest",
+		email: "guest@example.com",
+		password: "Password"
+	)
